54c789ec8ac820b9019320966b067a625ca3b94a
Felikso <aleksandrasniegucka@wp.pl>
2021-05-01 14:36:11 +0200

    setting up

--- src/data/MenuData.js
+++
@@ -1,6 +0,0 @@
-export const menuData = [
-    {title: "O nas", link: "/o-nas"},
-    {title: "Menu", link: "/menu"},
-    {title: "Kontakt", link: "/kontakt"},
-    {title: "Galeria", link: "/galeria"},
-]
